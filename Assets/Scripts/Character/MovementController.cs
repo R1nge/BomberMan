474d1fac49838997ed1e59182d29a36dd310a8cd
R1nge <r1ngegamedev@gmail.com>
2022-11-28 15:31:50 +0300

    Fixed spawning

--- Assets/Scripts/Character/MovementController.cs
+++ Assets/Scripts/Character/MovementController.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Netcode;
+﻿using System.Collections;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace Character
@@ -6,19 +7,29 @@ namespace Character
     public class MovementController : NetworkBehaviour
     {
         [SerializeField] private float speed;
-        [SerializeField] private bool canMove = true;
+        private bool _canMove;
         private Vector3 _moveDirection = Vector3.zero;
         private CharacterController _characterController;
 
-        private void Awake() => _characterController = GetComponent<CharacterController>();
+        private void Awake()
+        {
+            _characterController = GetComponent<CharacterController>();
+            StartCoroutine(Wait_C());
+        }
+
+        private IEnumerator Wait_C()
+        {
+            yield return new WaitForSeconds(1);
+            _canMove = true;
+        }
 
         private void Update()
         {
-            if (!IsOwner) return;
+            if (!IsOwner || !_canMove) return;
             Vector3 forward = transform.TransformDirection(Vector3.forward);
             Vector3 right = transform.TransformDirection(Vector3.right);
-            float curSpeedX = canMove ? speed * Input.GetAxis("Vertical") : 0;
-            float curSpeedY = canMove ? speed * Input.GetAxis("Horizontal") : 0;
+            float curSpeedX = _canMove ? speed * Input.GetAxis("Vertical") : 0;
+            float curSpeedY = _canMove ? speed * Input.GetAxis("Horizontal") : 0;
             _moveDirection = forward * curSpeedX + right * curSpeedY;
 
             _characterController.Move(_moveDirection * Time.deltaTime);
--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -3,36 +3,57 @@ using UnityEngine;
 
 public class PlayerSpawner : NetworkBehaviour
 {
-    [SerializeField] private Transform[] spawnPoints;
+    [SerializeField] private Transform[] spawnPositions;
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private NetworkVariable<int> players;
+    private NetworkList<Vector3> _positions;
 
+    private void Awake()
+    {
+        if (!NetworkManager.Singleton.IsHost)
+        {
+            players = new NetworkVariable<int>();
+            _positions = new NetworkList<Vector3>();
+        }
+    }
 
     public override void OnNetworkSpawn()
     {
-        base.OnNetworkSpawn();
-        SpawnPlayer(players.Value);
+        if (IsServer)
+        {
+            for (int i = 0; i < spawnPositions.Length; i++)
+            {
+                print("Position " + i);
+                _positions.Add(spawnPositions[i].position);
+            }
+        }
+
+        SpawnPlayer();
     }
 
-    private void SpawnPlayer(int index)
+    private void SpawnPlayer()
     {
-        if (!IsServer)
+        if (IsServer)
         {
-            SpawnPlayerServerRpc(index);
+            //Quick and dirty hack, but i'll leave it for now
+            var player = Instantiate(playerPrefab, spawnPositions[players.Value].position, Quaternion.identity);
+            player.GetComponent<NetworkObject>().SpawnWithOwnership((ulong) players.Value);
+            player.GetComponent<NetworkObject>().transform.position = spawnPositions[players.Value].position;
+
+            players.Value++;
         }
         else
         {
-            var player = Instantiate(playerPrefab, spawnPoints[index].position, Quaternion.identity);
-            //Quick and dirty hack, but i'll leave it for now
-            player.GetComponent<NetworkObject>().SpawnWithOwnership((ulong) players.Value);
-            players.Value++;
+            SpawnPlayerServerRpc();
         }
     }
 
     [ServerRpc(RequireOwnership = false)]
-    private void SpawnPlayerServerRpc(int index)
+    private void SpawnPlayerServerRpc() => SpawnPlayer();
+
+    public override void OnDestroy()
     {
-        SpawnPlayer(index);
-        players.Value++;
+        base.OnDestroy();
+        players.Dispose();
     }
 }

c178a1c0c5816412a463bd2a1d40d4474b777760
R1nge <r1ngegamedev@gmail.com>
2022-11-30 21:23:40 +0300

    Added walls

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -8,7 +8,7 @@ public class MapGenerator : NetworkBehaviour
     [SerializeField] private int minWidth, minHeight;
     [SerializeField] private int maxWidth, maxHeight;
     [SerializeField] private Transform parent;
-    [SerializeField] private GameObject tile, destructable, wall;
+    [SerializeField] private GameObject tile, destructable, borderWall, wall;
     [SerializeField] private Vector3 tileOffset, destructableOffset;
     [SerializeField] private float tileSize, destructableSize;
     private int _width, _height;
@@ -20,12 +20,13 @@ public class MapGenerator : NetworkBehaviour
     public override void OnNetworkSpawn()
     {
         if (!IsServer) return;
-        _width = Mathf.RoundToInt(Random.Range(minWidth, maxWidth));
-        _height = Mathf.RoundToInt(Random.Range(minHeight, maxHeight));
+        _width = GetRandomOddNumber(minWidth, maxWidth);
+        _height = GetRandomOddNumber(minHeight, maxHeight);
         _spawnPositions.SetSpawnPositions((_width - 1) * tileSize, (_height - 1) * tileSize);
         SpawnGrid();
-        SpawnDestructables();
         SpawnWalls();
+        SpawnDestructables();
+        SpawnBorders();
     }
 
     private void SpawnGrid()
@@ -49,6 +50,8 @@ public class MapGenerator : NetworkBehaviour
             {
                 _spawnObstacle = Mathf.FloorToInt(Random.Range(0, 2)) == 0;
                 if (!_spawnObstacle) continue;
+
+                if (x % 2 == 1 && z % 2 == 1) continue;
 
                 if (x == 0 && z == 0) continue;
                 if (x == 1 && z == 0) continue;
@@ -74,7 +77,7 @@ public class MapGenerator : NetworkBehaviour
         }
     }
 
-    private void SpawnWalls()
+    private void SpawnBorders()
     {
         for (int x = -1; x < _width + 1; x++)
         {
@@ -82,22 +85,44 @@ public class MapGenerator : NetworkBehaviour
             {
                 if (x < _width + 1 && z == _height)
                 {
-                    SpawnWall(x, z);
+                    SpawnBorderWall(x, z);
                 }
                 else if (x < _width + 1 && z == -1)
                 {
-                    SpawnWall(x, z);
+                    SpawnBorderWall(x, z);
                 }
                 else if (x == -1 && z < _height + 1)
                 {
-                    SpawnWall(x, z);
+                    SpawnBorderWall(x, z);
                 }
                 else if (x == _width && z < _height + 1)
                 {
-                    SpawnWall(x, z);
+                    SpawnBorderWall(x, z);
                 }
             }
         }
+    }
+
+    private void SpawnWalls()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                if (x % 2 == 1 && y % 2 == 1)
+                {
+                    SpawnWall(x, y);
+                }
+            }
+        }
+    }
+
+    private void SpawnBorderWall(int x, int z)
+    {
+        var inst = Instantiate(borderWall, (new Vector3(x, 0, z) + destructableOffset) * tileSize,
+            quaternion.identity);
+        inst.GetComponent<NetworkObject>().Spawn(true);
+        inst.transform.parent = parent;
     }
 
     private void SpawnWall(int x, int z)
@@ -106,5 +131,16 @@ public class MapGenerator : NetworkBehaviour
             quaternion.identity);
         inst.GetComponent<NetworkObject>().Spawn(true);
         inst.transform.parent = parent;
+    }
+
+    private int GetRandomOddNumber(int min, int max)
+    {
+        var num = Random.Range(min, max + 1);
+        if (num % 2 == 0)
+        {
+            num++;
+        }
+
+        return num;
     }
 }

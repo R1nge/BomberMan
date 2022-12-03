657c7f023ab784f0cb02dd8b282573416f8282da
R1nge <r1ngegamedev@gmail.com>
2022-12-03 05:16:46 +0300

    Made timer display minutes and second

--- Assets/Scripts/TimerUI.cs
+++ Assets/Scripts/TimerUI.cs
@@ -5,5 +5,11 @@ public class TimerUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMeshProUGUI;
 
-    public void UpdateUI(int value) => textMeshProUGUI.text = value.ToString();
+    public void UpdateUI(int value)
+    {
+        int minutes = Mathf.FloorToInt(value / 60f);
+        int seconds = Mathf.FloorToInt(value - minutes * 60);
+        string niceTime = $"{minutes:0}:{seconds:00}";
+        textMeshProUGUI.text = niceTime;
+    }
 }

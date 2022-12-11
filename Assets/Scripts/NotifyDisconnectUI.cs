b91f143730d2dd8b83b847c2847ccae8b88284c2
R1nge <r1ngegamedev@gmail.com>
2022-12-11 21:16:33 +0300

    Added on disconnect message

---
+++ Assets/Scripts/NotifyDisconnectUI.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+
+public class NotifyDisconnectUI : NetworkBehaviour
+{
+    [SerializeField] private TextMeshProUGUI disconnect;
+
+    private void Awake()
+    {
+        NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnect;
+    }
+
+    private void OnClientDisconnect(ulong obj)
+    {
+        var client = NetworkManager.Singleton.SpawnManager.GetPlayerNetworkObject(obj);
+        var nick = client.transform.GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>().text;
+        OnClientDisconnectedClientRpc(nick);
+    }
+
+    [ClientRpc]
+    private void OnClientDisconnectedClientRpc(NetworkString nick)
+    {
+        var str = nick + " has disconnected";
+        disconnect.text = str;
+        StartCoroutine(Clear_c());
+    }
+
+    private IEnumerator Clear_c()
+    {
+        yield return new WaitForSeconds(3);
+        disconnect.text = String.Empty;
+    }
+
+
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+        if (NetworkManager.Singleton == null) return;
+        NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnect;
+    }
+}

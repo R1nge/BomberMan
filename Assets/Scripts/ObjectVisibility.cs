33c3e8187847e437cc70f59500361acd97003157
R1nge <r1ngegamedev@gmail.com>
2022-12-14 10:30:31 +0300

    Object visibility hotfix

--- Assets/Scripts/ObjectVisibility.cs
+++ Assets/Scripts/ObjectVisibility.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using Unity.Netcode;
+﻿using Unity.Netcode;
 using UnityEngine;
 
 public class ObjectVisibility : NetworkBehaviour
 {
-    //TODO: Just use gameobject.SetActive(false) in clientRpc
-    //TODO: Or create a manager, that holds refs to players and all objects
-    //TODO: Then show for each client
     [SerializeField] private float maxDistance;
 
     private MeshRenderer _meshRenderer;
@@ -20,21 +16,6 @@ public class ObjectVisibility : NetworkBehaviour
     {
         if (IsServer)
         {
-<<<<<<< HEAD
-            NetworkManager.Singleton.OnClientConnectedCallback += obj => { CheckServerRpc(); };
-        }
-        else
-        {
-            NetworkManager.Singleton.SceneManager.OnLoadComplete += delegate { CheckServerRpc(); };
-        }
-    }
-
-    [ServerRpc(RequireOwnership = false)]
-    private void CheckServerRpc(ServerRpcParams rpcParams = default)
-    {
-<<<<<<< HEAD
-        if (NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject().IsOwner) return;
-=======
             NetworkManager.Singleton.OnClientConnectedCallback += obj => { Check(); };
         }
         else
@@ -48,45 +29,11 @@ public class ObjectVisibility : NetworkBehaviour
         var localId = NetworkManager.Singleton.LocalClientId;
         var player = NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject();
         if (!player.IsSpawned) return;
->>>>>>> 0572340 (Added object visibility)
-=======
-        var localId = rpcParams.Receive.SenderClientId;
-        var player = NetworkManager.Singleton.ConnectedClients[localId].PlayerObject;
-        if(player.IsOwnedByServer) return;
->>>>>>> 065287a (Kinda working)
         if (Vector3.Distance(
                 player.transform
                     .position,
                 transform.position) < maxDistance)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-            ShowServerRpc();
-=======
-            ShowClientRpc(localId);
->>>>>>> 065287a (Kinda working)
-        }
-        else
-        {
-            HideClientRpc(localId);
-        }
-    }
-
-    [ClientRpc]
-    private void ShowClientRpc(ulong client)
-    {
-        if (client != NetworkManager.LocalClientId) return;
-            if (!_meshRenderer.enabled)
-            {
-                _meshRenderer.enabled = true;
-                //NetworkObject.gameObject.SetActive(true);
-            }
-    }
-
-<<<<<<< HEAD
-    [ServerRpc(RequireOwnership = false)]
-    private void HideServerRpc(ServerRpcParams rpcParams = default)
-=======
             Show(localId);
         }
         else
@@ -105,38 +52,16 @@ public class ObjectVisibility : NetworkBehaviour
     }
 
     private void Hide(ulong client)
->>>>>>> 0572340 (Added object visibility)
-=======
-    [ClientRpc]
-    private void HideClientRpc(ulong client)
->>>>>>> 065287a (Kinda working)
     {
         if (client != NetworkManager.LocalClientId) return;
         if (_meshRenderer.enabled)
         {
-<<<<<<< HEAD
-<<<<<<< HEAD
-            _this.NetworkHide(rpcParams.Receive.SenderClientId);
-=======
             _meshRenderer.enabled = false;
->>>>>>> 0572340 (Added object visibility)
-=======
-            _meshRenderer.enabled = false;
-            //NetworkObject.gameObject.SetActive(false);
->>>>>>> 065287a (Kinda working)
         }
     }
 
     private void Update()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        //if (IsServer) return;
->>>>>>> 065287a (Kinda working)
-        CheckServerRpc();
-=======
         Check();
->>>>>>> 0572340 (Added object visibility)
     }
 }

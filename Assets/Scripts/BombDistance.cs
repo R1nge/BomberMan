0ada57b1600d48e7618c5871fceb9ddfeba38242
R1nge <r1ngegamedev@gmail.com>
2022-12-16 23:00:14 +0300

    Added max bomb distance

--- Assets/Scripts/BombDistance.cs
+++ Assets/Scripts/BombDistance.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class BombDistance : NetworkBehaviour
 {
+    [SerializeField] private int maxDistance;
     [SerializeField] private int increaseTime;
     [SerializeField] private NetworkVariable<int> distance;
     private BombDistanceUI _bombDistanceUI;
@@ -28,14 +29,13 @@ public class BombDistance : NetworkBehaviour
 
     private void OnTimePassed(int previousvalue, int newvalue)
     {
+        if (!IsServer) return;
+        if (distance.Value >= maxDistance) return;
         if (!_gameState.GameStarted.Value) return;
         if (_gameState.GameEnded.Value) return;
         if (newvalue % increaseTime == 0)
         {
-            if (IsServer)
-            {
-                distance.Value += 1;
-            }
+            distance.Value += 1;
         }
     }
 
